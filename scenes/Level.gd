249b704ea214a730176ea11dcc6a46a2fa35e0ae
Magoninho <magoninhogamer@gmail.com>
2023-10-05 23:59:25 -0300

    started level 2

--- scenes/Level.gd
+++ scenes/Level.gd
@@ -11,3 +11,9 @@ func _on_Pole_course_clear():
 	$Theme.stop()
 	$CourseClear.play()
 	
+
+
+func _on_CourseClear_finished():
+	Global.level += 1
+	Hud.update_hud()
+	get_tree().change_scene("res://scenes/Level" + str(Global.level) + ".tscn")
